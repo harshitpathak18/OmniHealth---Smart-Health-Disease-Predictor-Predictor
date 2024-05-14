0e558c094fc8f1d8364d6d10adc5f25ea15c45e2
Harshit Kumar Pathak <90919795+harshitpathak18@users.noreply.github.com>
2024-05-14 18:51:08 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -3,7 +3,9 @@ import os
 import streamlit as st
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyCn5BtlnY1xwzvQm_MgMlUIHIxJ_AyT7Tk")
+api_key = ""
+
+genai.configure(api_key=f"{api_key}")
 
 st.markdown("""
     <style>
